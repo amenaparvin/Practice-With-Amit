db52849eec7cd7524ddf40c3d5bc60bb384e2119
amenaparvin <amenaparvin2284@gmail.com>
2024-09-13 22:28:48 +0600

    create a joke caed

---
+++ index.js
@@ -0,0 +1,22 @@
+function getJoke(){
+    let config = {
+        headers: {
+            'Accept': 'application/json'
+        }
+    }
+
+    axios.get("https://icanhazdadjoke.com",config).then((res)=>{
+        console.log(res.data.joke)
+        document.getElementById("joke").innerHTML = res.data.joke
+    }).catch((err)=>{
+        console.error(err);
+        document.getElementById("joke").innerHTML = "An error occurred while fatching the joke."
+        
+
+    })
+        
+    
+}
+
+
+window.onload = getJoke
